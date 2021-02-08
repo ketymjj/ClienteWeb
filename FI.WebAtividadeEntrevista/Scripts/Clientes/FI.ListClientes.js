55bc2c15c8bc90718da2774b45062efc1ec3623c
ketlyn cristina neves <ketymjj@gmail.com>
2021-02-08 14:26:28 -0300

    Adicionar arquivos de projeto.

---
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/FI.ListClientes.js
@@ -0,0 +1,41 @@
+﻿
+$(document).ready(function () {
+
+    if (document.getElementById("gridClientes"))
+        $('#gridClientes').jtable({
+            title: 'Clientes',
+            paging: true, //Enable paging
+            pageSize: 5, //Set page size (default: 10)
+            sorting: true, //Enable sorting
+            defaultSorting: 'Nome ASC', //Set default sorting
+            actions: {
+                listAction: urlClienteList,
+            },
+            fields: {
+                Nome: {
+                    title: 'Nome',
+                    width: '35%'
+                },
+                Email: {
+                    title: 'Email',
+                    width: '35%'
+                },
+                Alterar: {
+                    title: 'Alterar',
+                    display: function (data) {
+                        return '<button onclick="window.location.href=\'' + urlAlteracao + '/' + data.record.Id + '\'" class="btn btn-primary btn-sm">Alterar</button>';
+                    }
+                },
+                Deletar: {
+                    title: 'Deletar',
+                    display: function (data) {
+                        return '<button onclick="window.location.href=\'' + urlDeletar + '/' + data.record.Id + '\'" class="btn btn-danger btn-sm">Deletar</button>';
+                    }
+                }
+            }
+        });
+
+    //Load student list from server
+    if (document.getElementById("gridClientes"))
+        $('#gridClientes').jtable('load');
+})
